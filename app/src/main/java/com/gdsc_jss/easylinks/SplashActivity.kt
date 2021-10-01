d328e71437c3ee655ff7f06825e97559d14da346
Anurajaj21 <anurajaj9690@gmail.com>
2021-10-01 22:57:00 +0530

    Login Screen and Signup Screen.

--- app/src/main/java/com/gdsc_jss/easylinks/SplashActivity.kt
+++ app/src/main/java/com/gdsc_jss/easylinks/SplashActivity.kt
@@ -1,13 +1,22 @@
 package com.gdsc_jss.easylinks
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
+    private val SPLASH_TIME = 1500
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        Handler(Looper.getMainLooper()).postDelayed({
+           val intent = Intent(applicationContext, LoginSignupActivity::class.java)
+           startActivity(intent)
+            finish()
+        }, SPLASH_TIME.toLong())
 
     }
 }
