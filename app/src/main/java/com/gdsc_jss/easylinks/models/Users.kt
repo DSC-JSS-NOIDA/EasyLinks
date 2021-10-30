1486becc336f15510258a8b425179d641719d1c1
karan7393065010 <karanverma9335@gmail.com>
2021-10-31 00:01:03 +0530

    Implemented code to create user in Firestore DB.

---
+++ app/src/main/java/com/gdsc_jss/easylinks/models/Users.kt
@@ -0,0 +1,15 @@
+package com.gdsc_jss.easylinks.models
+
+data class Users(
+    val id : String,
+    val name: String?,
+    val description: String?,
+    val github : String?,
+    val profilePicture : String?,
+    val behance : String?
+    )
+
+//data class Projects(
+//    val projectName : String?,
+//    val projectDesc : String?
+//)
