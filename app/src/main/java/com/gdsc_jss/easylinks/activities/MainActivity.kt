6552166c23037cd07cfe1980ab11452b9a6cc7e0
Anuraj Jain <69307834+Anurajaj21@users.noreply.github.com>
2021-10-31 16:22:31 +0530

    Add menu for logout (#32)

--- app/src/main/java/com/gdsc_jss/easylinks/activities/MainActivity.kt
+++ app/src/main/java/com/gdsc_jss/easylinks/activities/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.gdsc_jss.easylinks.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toolbar
 import com.gdsc_jss.easylinks.R
 import com.gdsc_jss.easylinks.fragments.ProfileFragment
 import com.gdsc_jss.easylinks.fragments.SearchFragment
@@ -11,6 +15,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
         supportFragmentManager
             .beginTransaction()
@@ -44,6 +50,24 @@ class MainActivity : AppCompatActivity() {
                 .replace(R.id.frame_layout, fragment, tag)
                 .commit()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.option_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.logout -> {
+                startActivity(Intent(applicationContext, AuthActivity::class.java))
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+
     }
 }
 
